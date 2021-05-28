8218cf3e2962ef6455e8a86f3f8430ea064bd6cd
danielethi <danielethi@outlook.com>
2021-05-27 21:55:59 -0500

    lab3 commits

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -45,49 +45,17 @@ console.log(car.acceleration); // 60
 
 //Q4 convert the following code to type script
 
-class BaseObject {
-    constructor(public width: number = 0, public length: number =0){
-    }
-    
-}
+var baseObject = { 
+	width: 0, 
+	length: 0 
+}; 
+var rectangle = Object.create(baseObject); 
 
-class Rectangle extends BaseObject {
-    calcSize():number{
-        return this.width * this.length; 
-    }
-}
+rectangle.width = 5; 
+rectangle.length = 2; 
 
+rectangle.calcSize = function() { 
+	return this.width * this.length; 
+}; 
 
-class Cube extends BaseObject {
-    public height: number;
-    constructor(width: number, length: number, height: number){
-        super(width, length);
-        this.height = height;
-    }
-
-    calcSize():number{
-        return this.width * this.length * this.height;
-    }
-}
-
-// interface BaseObject {
-//     width: number;
-//     length: number
-// }
-
-// class Rectangle implements BaseObject {
-
-//     public width: number;
-//     public length: number;
-
-//     constructor(width: number, length: number) {
-//         this.width = width;
-//         this.length = length;
-//     }
-//     calcSize():number {
-//         return this.width * this.length;
-//     }
-// }
-
-const rectangle = new Rectangle(5, 2);
 console.log(rectangle.calcSize()); // 10
